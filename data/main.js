a718781ef394124313f2b3214b6c9c6c31b5730d
Nathan Guerry <denito@gmail.com>
2022-05-30 19:54:36 +1000

    add sliders

--- data/main.js
+++ data/main.js
@@ -5,224 +5,260 @@ $(document).ready(function() {
     function processReceivedCommand(evt) {
         console.log(evt.data);
     }
+    $(".slider").each(function(index) {
+        noUiSlider.create(this, {
+            range: {
+                'min': $(this).data("min"),
+                'max': $(this).data("max")
+            },
+            format: wNumb({
+                decimals: 0
+            })
+        })
+        let slider = this
+        slider.noUiSlider.on('change', function(){
+            let data = {
+                'msgType': $(slider).data("msgType"),
+                'val': $(slider).val(),
+            };
+            ws.send(JSON.stringify(data));
+        });
+    })
 
-    $("#randomBtn").click(function() {
+    $(".control").click(function(event) {
         let data = {
-            'msgType': 1,
+            'msgType': $(event.currentTarget).data("msgType")
         }
         ws.send(JSON.stringify(data));
     })
 
-    $("#rainbowBtn").click(function() {
+
+    $("#.form-control").on("input", function(event) {
         let data = {
-            'msgType': 2,
-        }
+            'msgType': $(event.currentTarget).data("msgType"),
+            'val': $(event.currentTarget).val(),
+        };
         ws.send(JSON.stringify(data));
+        
     })
 
-    $("#tranquilityBtn").click(function() {
-        let data = {
-            'msgType': 3,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#randomBtn").click(function() {
+    //     let data = {
+    //         'msgType': 1,
+    //     }
+        
+    // })
 
-    $("#paletteBtn").click(function() {
-        let data = {
-            'msgType': 4,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#rainbowBtn").click(function() {
+    //     let data = {
+    //         'msgType': 2,
+    //     }
+        
+    // })
 
-    $("#tripperBtn").click(function() {
-        let data = {
-            'msgType': 5,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#tranquilityBtn").click(function() {
+    //     let data = {
+    //         'msgType': 3,
+    //     }
+        
+    // })
 
-    $("#antsBtn").click(function() {
-        let data = {
-            'msgType': 6,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#paletteBtn").click(function() {
+    //     let data = {
+    //         'msgType': 4,
+    //     }
+        
+    // })
 
-    $("#starsBtn").click(function() {
-        let data = {
-            'msgType': 7,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#tripperBtn").click(function() {
+    //     let data = {
+    //         'msgType': 5,
+    //     }
+        
+    // })
 
-    $("#houseLightsBtn").click(function() {
-        let data = {
-            'msgType': 8,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#antsBtn").click(function() {
+    //     let data = {
+    //         'msgType': 6,
+    //     }
+        
+    // })
 
-    $("#launchBtn").click(function() {
-        let data = {
-            'msgType': 9,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#starsBtn").click(function() {
+    //     let data = {
+    //         'msgType': 7,
+    //     }
+        
+    // })
 
-    $("#noiseBtn").click(function() {
-        let data = {
-            'msgType': 14,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#houseLightsBtn").click(function() {
+    //     let data = {
+    //         'msgType': 8,
+    //     }
+        
+    // })
 
-    $("#maskBtn").click(function() {
-        let data = {
-            'msgType': 15,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#launchBtn").click(function() {
+    //     let data = {
+    //         'msgType': 9,
+    //     }
+        
+    // })
 
-    $("#earthBtn").click(function() {
-        let data = {
-            'msgType': 20,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#noiseBtn").click(function() {
+    //     let data = {
+    //         'msgType': 14,
+    //     }
+        
+    // })
 
-    $("#fireBtn").click(function() {
-        let data = {
-            'msgType': 21,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#maskBtn").click(function() {
+    //     let data = {
+    //         'msgType': 15,
+    //     }
+        
+    // })
 
-    $("#airBtn").click(function() {
-        let data = {
-            'msgType': 22,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#earthBtn").click(function() {
+    //     let data = {
+    //         'msgType': 20,
+    //     }
+        
+    // })
 
-    $("#waterBtn").click(function() {
-        let data = {
-            'msgType': 23,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#fireBtn").click(function() {
+    //     let data = {
+    //         'msgType': 21,
+    //     }
+        
+    // })
 
-    $("#metalBtn").click(function() {
-        let data = {
-            'msgType': 24,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#airBtn").click(function() {
+    //     let data = {
+    //         'msgType': 22,
+    //     }
+        
+    // })
 
-    $("#stepInput").on("input", function() {
-        let data = {
-            'msgType': 10,
-            'val': $("#stepInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#waterBtn").click(function() {
+    //     let data = {
+    //         'msgType': 23,
+    //     }
+        
+    // })
 
-    $("#fadeInput").on("input", function() {
-        let data = {
-            'msgType': 11,
-            'val': $("#fadeInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#metalBtn").click(function() {
+    //     let data = {
+    //         'msgType': 24,
+    //     }
+        
+    // })
 
-    $("#brightnessInput").on("input", function() {
-        let data = {
-            'msgType': 12,
-            'val': $("#brightnessInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#stepInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 10,
+    //         'val': $("#stepInput").val(),
+    //     }
+        
+    // })
 
-    $("#ripplesInput").on("input", function() {
-        let data = {
-            'msgType': 13,
-            'val': $("#ripplesInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#fadeInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 11,
+    //         'val': $("#fadeInput").val(),
+    //     }
+        
+    // })
 
-    $("#nlInput").on("input", function() {
-        let data = {
-            'msgType': 16,
-            'val': $("#nlInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#brightnessInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 12,
+    //         'val': $("#brightnessInput").val(),
+    //     }
+        
+    // })
 
-    $("#nsInput").on("input", function() {
-        let data = {
-            'msgType': 17,
-            'val': $("#nsInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#ripplesInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 13,
+    //         'val': $("#ripplesInput").val(),
+    //     }
+        
+    // })
 
-    $("#nflInput").on("input", function() {
-        let data = {
-            'msgType': 18,
-            'val': $("#nflInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#nlInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 16,
+    //         'val': $("#nlInput").val(),
+    //     }
+        
+    // })
 
-    $("#nfsInput").on("input", function() {
-        let data = {
-            'msgType': 19,
-            'val': $("#nfsInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#nsInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 17,
+    //         'val': $("#nsInput").val(),
+    //     }
+        
+    // })
 
-    $("#airlInput").on("input", function() {
-        let data = {
-            'msgType': 25,
-            'val': $("#airlInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#nflInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 18,
+    //         'val': $("#nflInput").val(),
+    //     }
+        
+    // })
 
-    $("#airsInput").on("input", function() {
-        let data = {
-            'msgType': 26,
-            'val': $("#airsInput").val(),
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#nfsInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 19,
+    //         'val': $("#nfsInput").val(),
+    //     }
+        
+    // })
 
-    $("#ripplesBtn").click(function() {
-        let data = {
-            'msgType': 28,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#airlInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 25,
+    //         'val': $("#airlInput").val(),
+    //     }
+        
+    // })
 
-    $("#tailBtn").click(function() {
-        let data = {
-            'msgType': 29,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#airsInput").on("input", function() {
+    //     let data = {
+    //         'msgType': 26,
+    //         'val': $("#airsInput").val(),
+    //     }
+        
+    // })
 
-    $("#blenderBtn").click(function() {
-        let data = {
-            'msgType': 30,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#ripplesBtn").click(function() {
+    //     let data = {
+    //         'msgType': 28,
+    //     }
+        
+    // })
 
-    $("#flashBtn").click(function() {
-        let data = {
-            'msgType': 31,
-        }
-        ws.send(JSON.stringify(data));
-    })
+    // $("#tailBtn").click(function() {
+    //     let data = {
+    //         'msgType': 29,
+    //     }
+        
+    // })
+
+    // $("#blenderBtn").click(function() {
+    //     let data = {
+    //         'msgType': 30,
+    //     }
+        
+    // })
+
+    // $("#flashBtn").click(function() {
+    //     let data = {
+    //         'msgType': 31,
+    //     }
+        
+    // })
 })
