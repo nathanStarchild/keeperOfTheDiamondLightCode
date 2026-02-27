aff41bf5a27ed944808413e1f06ce306caf2edba
Nathan Starchild <nathan@starchildart.com>
2026-02-27 12:49:46 +1100

    doof stick and burn barrel .h files

---
+++ mg_burn_barrels.h
@@ -0,0 +1,31 @@
+#include "pinDefinitions.h"
+
+#define ROLE "burnBarrels"
+
+const uint16_t stripLength = 57;
+const uint8_t nStrips = 8;
+const uint16_t num_leds = stripLength * nStrips;
+
+#define LEDS_OUTPUT1 stripLength * nStrips
+#define NUM_OUTPUTS 1
+#define NUM_LEDS stripLength * nStrips
+CRGB leds[NUM_LEDS];
+CRGB oldLeds[NUM_LEDS];
+CRGB outLeds[NUM_LEDS];
+
+int directionLR[nStrips] = {-1, -1, -1, -1, -1, -1, -1, -1,};
+int directionUD[nStrips] = {1, -1, 1, -1, 1, -1, 1, -1};
+int directionIO[nStrips] = {1, -1, 1, -1, 1, -1, 1, -1};
+int stripDirection[nStrips] =  {1, -1, 1, -1, 1, -1, 1, -1};
+int stripZ[nStrips] = {3, 3, 3, 3, 3, 3, 3, 3};
+uint16_t audienceSpot = 1;
+uint16_t sweepSpot = 1;
+uint16_t element = 0;
+
+void elementSetup(){
+    FastLED.addLeds<WS2813, DATA_PIN_2, RGB>(outLeds, 0, LEDS_OUTPUT1); 
+//    FastLED.addLeds<WS2813, DATA_PIN_1, RGB>(outLeds, 0, 600); 
+//    FastLED.addLeds<WS2813, DATA_PIN_2, RGB>(outLeds, 600, 360); 
+
+    ArduinoOTA.setHostname("burn_barrels");
+}
---
+++ mg_doof_stick_2.h
@@ -0,0 +1,38 @@
+#define ESP8266 true
+#include "pinDefinitions.h"
+
+#define ROLE "doofStick"
+#define NUMBER 2
+
+const uint16_t stripLength = 100;
+const uint8_t nStrips = 1;
+const uint16_t num_leds = stripLength * nStrips;
+
+#define LEDS_OUTPUT1 stripLength * 1
+// #define LEDS_OUTPUT2 stripLength * 4
+// #define LEDS_OUTPUT3 stripLength * 4
+#define NUM_OUTPUTS 1
+#define NUM_LEDS stripLength * nStrips
+CRGB leds[NUM_LEDS];
+CRGB oldLeds[NUM_LEDS];
+CRGB outLeds[NUM_LEDS];
+
+int directionLR[nStrips] = {1};
+int directionUD[nStrips] = {-1};
+int directionIO[nStrips] = {1};
+int stripDirection[nStrips] =  {-1};
+uint16_t audienceSpot = 1;
+uint16_t sweepSpot = NUMBER + 3;
+uint16_t element = 0;
+
+void elementSetup(){
+    FastLED.addLeds<WS2813, DATA_PIN_1, RGB>(outLeds, 0, LEDS_OUTPUT1); 
+    // FastLED.addLeds<WS2813, DATA_PIN_2, RGB>(outLeds, LEDS_OUTPUT1, LEDS_OUTPUT2); 
+    // FastLED.addLeds<WS2813, DATA_PIN_3, RGB>(outLeds, LEDS_OUTPUT1 + LEDS_OUTPUT2, LEDS_OUTPUT3); 
+//    FastLED.addLeds<WS2813, DATA_PIN_1, RGB>(outLeds, 0, 600); 
+//    FastLED.addLeds<WS2813, DATA_PIN_2, RGB>(outLeds, 600, 360); 
+
+    char hostname[32];
+    snprintf(hostname, sizeof(hostname), "%s_%d", ROLE, NUMBER);
+    ArduinoOTA.setHostname(hostname);
+}
